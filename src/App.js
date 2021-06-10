d44ccbf2ced1b9695aca704339ea0a6f036b8862
Lixuan Luo <lixuanluo@hotmail.com>
2021-06-10 17:44:38 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import "./App.css";
 import Header from "./components/Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">
-    <Header/>
-  </div>;
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login page</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <h1>Home page</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
